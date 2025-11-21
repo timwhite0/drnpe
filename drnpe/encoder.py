38d7da32fcfd6067a78db9550c0c64b1f386ab83
Tim White <twhit@umich.edu>
2025-11-21 00:29:33 -0500

    Allow training with either primal or dual objective

--- drnpe/encoder.py
+++ drnpe/encoder.py
@@ -9,6 +9,7 @@ from torch.distributions import Normal
 class Encoder(lightning.LightningModule):
     def __init__(
         self,
+        objective: str,
         kl_ball_threshold: float,
         initial_lambda: float,
         x_dim: int,
@@ -17,15 +18,21 @@ class Encoder(lightning.LightningModule):
     ):
         super().__init__()
 
+        self.objective = objective
         self.kl_ball_threshold = kl_ball_threshold
         self.initial_lambda = initial_lambda
         self.x_dim = x_dim
         self.num_hidden_channels = num_hidden_channels
         self.lr = lr
 
-        self.lambda_softplus_inverse = torch.nn.Parameter(
-            torch.log(torch.exp(torch.tensor(self.initial_lambda)) - 1.0)
+        lambda_softplus_inverse = torch.log(
+            torch.exp(torch.tensor(self.initial_lambda)) - 1.0
         )
+        if objective == "primal":
+            self.lambda_softplus_inverse = lambda_softplus_inverse
+        elif objective == "dual":
+            self.lambda_softplus_inverse = torch.nn.Parameter(lambda_softplus_inverse)
+
         self.net = LocationScaleNet(
             x_dim=self.x_dim, num_hidden_channels=self.num_hidden_channels
         )
@@ -39,12 +46,15 @@ class Encoder(lightning.LightningModule):
         logq = Normal(mu, sigma).log_prob(z)
         lam = F.softplus(self.lambda_softplus_inverse) + 1e-8
 
-        logq_scaled = -(1.0 / lam) * logq
+        logw = -(1.0 / lam) * logq
 
-        m = torch.max(logq_scaled)
-        log_exp_avg = m + torch.log(torch.mean(torch.exp(logq_scaled - m)))
+        m = torch.max(logw)
+        log_Ew = m + torch.log(torch.mean(torch.exp(logw - m)))
 
-        loss = lam * (log_exp_avg + self.kl_ball_threshold)
+        if self.objective == "primal":
+            loss = ((logw - log_Ew).exp() * -logq).mean()
+        elif self.objective == "dual":
+            loss = lam * (log_Ew + self.kl_ball_threshold)
 
         self.log(
             f"{mode}_loss",
