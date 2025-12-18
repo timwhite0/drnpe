ec9dfbd779b0e79e57f807b9020639125fd6f89b
Tim White <twhit@umich.edu>
2025-12-18 10:51:38 -0500

    Separate encoders for NPE and DRNPE

--- drnpe/data.py
+++ drnpe/data.py
@@ -1,5 +1,4 @@
 import lightning
-import torch
 from torch import Tensor
 from torch.distributions import Normal
 from torch.utils.data import DataLoader, TensorDataset, random_split
@@ -9,9 +8,7 @@ class GaussianDataModule(lightning.LightningDataModule):
     def __init__(
         self,
         prior_stdev: float,
-        z_noise_stdev: float,
         likelihood_stdev: float,
-        x_noise_stdev: float,
         num_observations: int,
         num_batches: int,
         batch_size: int,
@@ -20,25 +17,21 @@ class GaussianDataModule(lightning.LightningDataModule):
     ):
         super().__init__()
         self.prior_stdev = prior_stdev
-        self.z_noise_stdev = z_noise_stdev
         self.likelihood_stdev = likelihood_stdev
-        self.x_noise_stdev = x_noise_stdev
         self.num_observations = num_observations
         self.num_batches = num_batches
         self.batch_size = batch_size
         self.train_split = train_split
         self.val_split = val_split
 
-    def generate_dataset(self, num_samples, z_noise_stdev, x_noise_stdev):
+    def generate_dataset(self, num_samples):
         z = Normal(0.0, self.prior_stdev).sample([num_samples])
         x = (
             Normal(z, self.likelihood_stdev)
             .sample([self.num_observations])
             .permute(1, 0)
         )
-        z_noised = z + z_noise_stdev * torch.randn_like(z)
-        x_noised = x + x_noise_stdev * torch.randn_like(x)
-        return TensorDataset(Tensor(z_noised), Tensor(x_noised))
+        return TensorDataset(Tensor(z), Tensor(x))
 
     def setup(self, stage: str):
         size_total = self.num_batches * self.batch_size
@@ -47,14 +40,10 @@ class GaussianDataModule(lightning.LightningDataModule):
         size_test = size_total - size_train - size_val
 
         # Generate training dataset with noise
-        self.train_dataset = self.generate_dataset(
-            size_train, self.z_noise_stdev, self.x_noise_stdev
-        )
+        self.train_dataset = self.generate_dataset(size_train)
 
         # Generate validation and test datasets without noise
-        valtest = self.generate_dataset(
-            size_val + size_test, z_noise_stdev=0.0, x_noise_stdev=0.0
-        )
+        valtest = self.generate_dataset(size_val + size_test)
         self.val_dataset, self.test_dataset = random_split(
             valtest, [size_val, size_test]
         )
--- drnpe/encoder.py
+++ drnpe/encoder.py
@@ -6,33 +6,12 @@ from torch import optim
 from torch.distributions import Normal
 
 
-class Encoder(lightning.LightningModule):
-    def __init__(
-        self,
-        objective: str,
-        kl_ball_threshold: float,
-        initial_lambda: float,
-        x_dim: int,
-        num_hidden_channels: int,
-        lr: float,
-    ):
+class EncoderNPE(lightning.LightningModule):
+    def __init__(self, x_dim: int, num_hidden_channels: int, lr: float):
         super().__init__()
-
-        self.objective = objective
-        self.kl_ball_threshold = kl_ball_threshold
-        self.initial_lambda = initial_lambda
         self.x_dim = x_dim
         self.num_hidden_channels = num_hidden_channels
         self.lr = lr
-
-        lambda_softplus_inverse = torch.log(
-            torch.exp(torch.tensor(self.initial_lambda)) - 1.0
-        )
-        if objective == "primal":
-            self.lambda_softplus_inverse = lambda_softplus_inverse
-        elif objective == "dual":
-            self.lambda_softplus_inverse = torch.nn.Parameter(lambda_softplus_inverse)
-
         self.net = LocationScaleNet(
             x_dim=self.x_dim, num_hidden_channels=self.num_hidden_channels
         )
@@ -44,17 +23,8 @@ class Encoder(lightning.LightningModule):
         sigma = torch.exp(log_sigma)
 
         logq = Normal(mu, sigma).log_prob(z)
-        lam = F.softplus(self.lambda_softplus_inverse) + 1e-8
 
-        logw = -(1.0 / lam) * logq
-
-        m = torch.max(logw)
-        log_Ew = m + torch.log(torch.mean(torch.exp(logw - m)))
-
-        if self.objective == "primal":
-            loss = ((logw - log_Ew).exp() * -logq).mean()
-        elif self.objective == "dual":
-            loss = lam * (log_Ew + self.kl_ball_threshold)
+        loss = -logq.mean()
 
         self.log(
             f"{mode}_loss",
@@ -78,3 +48,78 @@ class Encoder(lightning.LightningModule):
 
     def configure_optimizers(self):
         return optim.Adam(self.parameters(), lr=self.lr)
+
+
+class EncoderDRNPE(EncoderNPE):
+    def __init__(
+        self,
+        kl_ball_threshold: float,
+        initial_lambda: float,
+        x_dim: int,
+        z_noise_stdev: float,
+        x_noise_stdev: float,
+        num_hidden_channels: int,
+        lr: float,
+        objective: str = "drnpe primal",
+    ):
+        super().__init__(x_dim=x_dim, num_hidden_channels=num_hidden_channels, lr=lr)
+        self.objective = objective
+        self.kl_ball_threshold = kl_ball_threshold
+        self.initial_lambda = initial_lambda
+        self.z_noise_stdev = z_noise_stdev
+        self.x_noise_stdev = x_noise_stdev
+        lambda_softplus_inverse = torch.log(
+            torch.exp(torch.tensor(self.initial_lambda)) - 1.0
+        )
+        if objective == "drnpe primal":
+            self.lambda_softplus_inverse = lambda_softplus_inverse
+        elif objective == "drnpe dual":
+            self.lambda_softplus_inverse = torch.nn.Parameter(lambda_softplus_inverse)
+
+    def compute_loss(self, batch, batch_idx, mode):
+        z, x = batch
+        if mode == "train":
+            z = z + self.z_noise_stdev * torch.randn_like(z)
+            x = x + self.x_noise_stdev * torch.randn_like(x)
+
+        mu, log_sigma = self.net(x)
+        sigma = torch.exp(log_sigma)
+
+        logq = Normal(mu, sigma).log_prob(z)
+
+        lam = F.softplus(self.lambda_softplus_inverse) + 1e-8
+        logw = -(1.0 / lam) * logq
+        m = torch.max(logw)
+        log_Ew = m + torch.log(torch.mean(torch.exp(logw - m)))
+
+        if self.objective == "drnpe primal":
+            loss = ((logw - log_Ew).exp() * -logq).mean()
+        elif self.objective == "drnpe dual":
+            loss = lam * (log_Ew + self.kl_ball_threshold)
+            self.log(
+                f"{mode}_lambda",
+                lam,
+                on_step=False,
+                on_epoch=True,
+                logger=True,
+                prog_bar=True,
+            )
+
+        self.log(
+            f"{mode}_loss",
+            loss,
+            on_step=False,
+            on_epoch=True,
+            logger=True,
+            prog_bar=True,
+        )
+        self.log(
+            f"{mode}_logEw",
+            log_Ew,
+            on_step=False,
+            on_epoch=True,
+            logger=True,
+            prog_bar=True,
+        )
+
+        return loss
